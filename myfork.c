94225072b12e2b2c6ff303ea351ee43162e04399
JohnathanFL <jfl747076@gmail.com>
2019-11-08 17:54:52 -0600

    Fix myfork.c so it can terminate and so it loops
    Apparently GCC was so darned smart it optimized out the entire loop before.
    getpid() adds an element of randomness that it can't account for.

--- myfork.c
+++ myfork.c
@@ -1,8 +1,8 @@
 #include "user.h"
 
-void consume(int t) { t += 1; }
 
 int main(int argc, char** argv) {
+  // Honestly not sure what N is supposed to do in this assignment
   int n = atoi(argv[1]);
   for (int k = 0; k < n; k++) {
     int id = fork();
@@ -11,8 +11,9 @@ int main(int argc, char** argv) {
     } else if (id == 0) {
       // printf(1, "Child checking in\n");
       int tmp = 1;
-      for (int i = 0; i < 10000000000; i++) tmp = i * 1 / 10 * 50;
-      consume(tmp);
+      for (int i = 0; i < 1000000000; i++) tmp = i * getpid() / 10 * 50;
+
+      printf(1, "%d\n", tmp);
       break;
     } else {
       // printf(1, "Parent %d creating proc %d\n", getpid(), id);
