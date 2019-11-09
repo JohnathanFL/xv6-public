8cee266494f94f3dea8deb788e252a02077fa9ec
JohnathanFL <jfl747076@gmail.com>
2019-11-08 19:29:51 -0600

    Prettify the procdumper

--- proc.c
+++ proc.c
@@ -461,24 +461,17 @@ int kill(int pid) {
 void procdump(void) {
   static char* states[] = { [UNUSED] "unused",   [EMBRYO] "embryo",  [SLEEPING] "sleep ",
                             [RUNNABLE] "runble", [RUNNING] "run   ", [ZOMBIE] "zombie" };
-  int          i;
   struct proc* p;
   char*        state;
-  uint         pc[10];
 
-  cprintf("\n");
+  cprintf("\nPID\tSTATE\tPRIOR\tNAME\n");
   for (p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
     if (p->state == UNUSED) continue;
     if (p->state >= 0 && p->state < NELEM(states) && states[p->state])
       state = states[p->state];
     else
       state = "???";
-    cprintf("PID=%d STATE=%s PRIOR=%d NAME=%s", p->pid, state, p->priority, p->name);
-    if (p->state == SLEEPING) {
-      getcallerpcs((uint*)p->context->ebp + 2, pc);
-      for (i = 0; i < 10 && pc[i] != 0; i++) cprintf(" %p", pc[i]);
-    }
-    cprintf("\n");
+    cprintf("%d\t%s\t%d\t%s\n", p->pid, state, p->priority, p->name);
   }
 }
 
