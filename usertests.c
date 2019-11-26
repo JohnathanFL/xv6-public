e0eccbe22792ae20999abd7369192979e6562546
JohnathanFL <jfl747076@gmail.com>
2019-11-26 13:44:53 -0600

    Ignore preempt and part of memtest
    Preempt can never pass because the priority scheduler the professor
    asked us to implement can never preempt without a priority change.
    
    The commented memtest portion can never pass because malloc doesn't
    know about our deferred page allocation, so it will allocate itself
    out of a job. This could be fixed if we added page eviction, but
    that's not part of the assignment.

--- usertests.c
+++ usertests.c
@@ -398,13 +398,15 @@ void exitwait(void) {
 }
 
 void mem(void) {
-  void *m1, *m2;
+  void *m1; ////, *m2;
   int   pid, ppid;
 
   printf(1, "mem test\n");
   ppid = getpid();
   if ((pid = fork()) == 0) {
     m1 = 0;
+    /****
+     **** This can never pass since malloc doesn't know about deferred alloc and we can't evict.
     while ((m2 = malloc(10001)) != 0) {
       *(char**)m2 = m1;
       m1          = m2;
@@ -413,7 +415,7 @@ void mem(void) {
       m2 = *(char**)m1;
       free(m1);
       m1 = m2;
-    }
+    }****/
     m1 = malloc(1024 * 20);
     if (m1 == 0) {
       printf(1, "couldn't allocate mem?!!\n");
@@ -1682,7 +1684,7 @@ int main(int argc, char* argv[]) {
 
   mem();
   pipe1();
-  preempt();
+  ////preempt();
   exitwait();
 
   rmdot();
